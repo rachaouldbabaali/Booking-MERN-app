7e1ceb6dff223a9fcafa635646917cf160f973f3
Racha OULDBABAALI <rachaould@gmail.com>
2024-02-21 01:02:35 +0100

    Add room routes

--- api/routes/room.js
+++ api/routes/room.js
@@ -1,9 +1,24 @@
-import express from 'express';
+import express from "express";
+import Room from "../models/Room.js";
+import { createRoom, updateRoom, deleteRoom, getRooms, getRoomById } from "../controllers/room.js";
+import { verifyToken, verifyUser, verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.post('/rooms', (req, res) => {
-    res.send('Hello World! from rooms');
-    });
+// Create a new room
+router.post("/:hotelId", verifyAdmin, createRoom);
+
+// Update a room
+router.put("/:id", verifyAdmin, updateRoom);
+
+// delete a room
+router.delete("/:id/:hotelId", verifyAdmin, deleteRoom);
+
+// get all rooms
+router.get("/", getRooms);
+
+// get a room by id
+router.get("/:id", getRoomById);
+
 
 export default router;
