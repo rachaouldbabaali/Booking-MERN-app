d3455eb2b880137d088048e09e08af6cf634728c
Racha OULDBABAALI <rachaould@gmail.com>
2024-02-21 01:03:10 +0100

    Update users routes and verifyToken

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -15,21 +15,21 @@ export const verifyToken = (req, res, next) => {
 };
 
 export const verifyUser = (req, res, next) => {
-    verifyToken(req, res,  () => {
-        if (req.user._id === req.params.id || req.user.isAdmin) {
-        next();
-        } else {
-        return next(createError(403, "You are not authorized!"));
-        }
-    });
-    };
+  verifyToken(req, res, next, () => {
+    if (req.user._id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      return next(createError(403, "You are not authorized!"));
+    }
+  });
+};
 
-    export const verifyAdmin = (req, res, next) => {
-        verifyToken(req, res, () => {
-          if (req.user.isAdmin) {
-            next();
-          } else {
-            return next(createError(403, "You are not authorized!"));
-          }
-        });
-      };
+export const verifyAdmin = (req, res, next) => {
+  verifyToken(req, res, next, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      return next(createError(403, "You are not authorized!"));
+    }
+  });
+};
